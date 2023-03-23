554924fcc9fcbb1fa55f71d6a6b36d02a4289520
FranklinThree <1765370549@qq.com>
2023-03-23 09:52:15 +0800

    0223-uptodate

--- src/main/resources/secret/mysql.sql
+++ src/main/resources/secret/mysql.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS account_book DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+use account_book;
 
 -- 创建用户表
 CREATE TABLE user (
@@ -6,6 +7,8 @@ CREATE TABLE user (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
+    wx_id VARCHAR(50) NOT NULL,
+    qq_id VARCHAR(50) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
 
@@ -20,3 +23,36 @@ CREATE TABLE session (
     updated_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 );
+
+
+-- 创建账单表
+
+CREATE TABLE bill (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    type_id INT(11) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- 创建账单类型表
+CREATE TABLE bill_type (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+#     user_id INT(11) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- 创建图片资源表
+CREATE TABLE picture (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    data MEDIUMBLOB NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+);
+
